2e937ddf20ef7f90df79e5b1397a08e78b250b06
thomaszwagerman <thozwa@bas.ac.uk>
2025-12-11 23:38:30 +0000

    account for SIC being None

--- polar_route/vessel_performance/vessels/abstract_glider.py
+++ polar_route/vessel_performance/vessels/abstract_glider.py
@@ -108,7 +108,7 @@ class AbstractGlider(AbstractVessel):
             Returns:
                 ext_ice (bool): boolean that is True if the cell is inaccessible due to ice
         """
-        if 'SIC' not in cellbox.agg_data:
+        if "SIC" not in cellbox.agg_data or cellbox.agg_data["SIC"] is None:
             logging.debug(f"No sea ice concentration data in cell {cellbox.id}")
             ext_ice = False
         else:
