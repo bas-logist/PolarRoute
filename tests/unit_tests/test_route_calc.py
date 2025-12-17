c88e5c5a1fd8affc62594ecf12fc01132e9b2848
thomaszwagerman <thozwa@bas.ac.uk>
2025-12-17 22:43:56 +0000

    also parametrize unit tests

--- tests/unit_tests/test_route_calc.py
+++ tests/unit_tests/test_route_calc.py
@@ -1,45 +1,17 @@
-import unittest
+import pytest
 import numpy as np
 from polar_route.route_planner.crossing import traveltime_in_cell
 
 
-class TestRouteCalc(unittest.TestCase):
-
-    def test_traveltime_in_cell(self):
-        bx = 30
-        by = 15
-        cx = 2
-        cy = -4
-        s = np.sqrt(65)
-
-        tt = traveltime_in_cell(bx, by, cx, cy, s)
-
-        expected_tt = 5.0
-
-        self.assertAlmostEqual(tt, expected_tt, places=5)
-
-    def test_traveltime_in_cell_reverse_current(self):
-        bx = 30
-        by = 15
-        cx = -2
-        cy = -4
-        s = np.sqrt(65)
-
-        tt = traveltime_in_cell(bx, by, cx, cy, s)
-
-        expected_tt = 8.33333
-
-        self.assertAlmostEqual(tt, expected_tt, places=5)
-
-    def test_traveltime_in_cell_slow_vessel(self):
-        bx = 30
-        by = 15
-        cx = 2
-        cy = -4
-        s = 5
-
-        tt = traveltime_in_cell(bx, by, cx, cy, s)
-
-        expected_tt = 15.0
-
-        self.assertAlmostEqual(tt, expected_tt, places=5)
+@pytest.mark.parametrize(
+    "bx, by, cx, cy, speed, expected_tt, test_id",
+    [
+        (30, 15, 2, -4, np.sqrt(65), 5.0, "normal_current"),
+        (30, 15, -2, -4, np.sqrt(65), 8.33333, "reverse_current"),
+        (30, 15, 2, -4, 5, 15.0, "slow_vessel"),
+    ],
+)
+def test_traveltime_in_cell(bx, by, cx, cy, speed, expected_tt, test_id):
+    """Test travel time calculation in cell with various conditions."""
+    tt = traveltime_in_cell(bx, by, cx, cy, speed)
+    assert tt == pytest.approx(expected_tt, abs=1e-5)
